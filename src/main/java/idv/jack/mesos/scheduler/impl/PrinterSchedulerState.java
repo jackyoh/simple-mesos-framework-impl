abb8d49bdfb3718de1a4f932a1335cda4994f24b
jack <jack@is-land.com.tw>
2016-09-17 12:54:57 +0800

    Scheduler add resourceOffers and statusUpdate function

--- src/main/java/idv/jack/mesos/scheduler/impl/PrinterSchedulerState.java
+++ src/main/java/idv/jack/mesos/scheduler/impl/PrinterSchedulerState.java
@@ -1,5 +1,6 @@
 package idv.jack.mesos.scheduler.impl;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,6 +11,7 @@ import org.apache.mesos.Protos.Offer;
 import org.apache.mesos.Protos.OfferID;
 import org.apache.mesos.Protos.SlaveID;
 import org.apache.mesos.Protos.TaskStatus;
+import org.apache.mesos.Protos;
 import org.apache.mesos.Scheduler;
 import org.apache.mesos.SchedulerDriver;
 
@@ -17,9 +19,11 @@ public class PrinterSchedulerState implements Scheduler{
 
 	private LinkedList<String> tasks;
 	private int numTasks;
+	private int tasksSubmitted;
+	private int tasksCompleted;
 	private int startValue = 1;
 	private int endValue = 1000;
-	
+	private int totalResult = 0;
 	public PrinterSchedulerState(String[] args, int numTasks){
 		this.numTasks = numTasks;
 		tasks = new LinkedList<String>();
@@ -28,7 +32,7 @@ public class PrinterSchedulerState implements Scheduler{
 		int count = 1;
 		for(int i = startValue ; i < endValue ; i += avgValue){
 			int stopValue = avgValue * count;
-			tasks.add(i + "," + stopValue);
+			tasks.add(i + " " + stopValue);
 			count++;
 		}
 		
@@ -49,8 +53,26 @@ public class PrinterSchedulerState implements Scheduler{
 	public void resourceOffers(SchedulerDriver driver, List<Offer> offers) {
 		System.out.println("Scheduler received offers " + offers.size());
 		for(Offer offer : offers){
-			if(offers.size() > 0){
+			if(tasks.size() > 0){
+				tasksSubmitted++;
+				String task = tasks.remove();
+				Protos.TaskID taskID = Protos.TaskID.newBuilder().setValue(String.valueOf(tasksSubmitted)).build();
+				System.out.println("lAUNCHING TASK " + taskID.getValue() + " on slave " + offer.getSlaveId().getValue() + " with " + task);
+				Protos.ExecutorInfo executor = Protos.ExecutorInfo.newBuilder()
+											.setExecutorId(Protos.ExecutorID.newBuilder().setValue(String.valueOf(tasksSubmitted)))
+											.setCommand(createCommand(task))
+											.setName("Number total").build();
+
 				
+				Protos.TaskInfo taskInfo = Protos.TaskInfo.newBuilder()
+						.setName("Add Number Task-" + taskID.getValue())
+						.setExecutor(Protos.ExecutorInfo.newBuilder(executor))
+						.addResources(Protos.Resource.newBuilder().setName("cpus").setType(Protos.Value.Type.SCALAR).setScalar(Protos.Value.Scalar.newBuilder().setValue(1)))
+						.addResources(Protos.Resource.newBuilder().setName("mem").setType(Protos.Value.Type.SCALAR).setScalar(Protos.Value.Scalar.newBuilder().setValue(128)))
+						.setSlaveId(offer.getSlaveId())
+						.build();
+
+				driver.launchTasks(Collections.singletonList(offer.getId()), Collections.singletonList(taskInfo));
 			}
 			
 		}
@@ -66,9 +88,22 @@ public class PrinterSchedulerState implements Scheduler{
 	}
 
 	@Override
-	public void statusUpdate(SchedulerDriver driver, TaskStatus status) {
-		System.out.println("Status update: task " + status.getTaskId().getValue() + " state is " + status.getState());
+	public void statusUpdate(SchedulerDriver driver, TaskStatus taskStatus) {
+		System.out.println("Status update: task " + taskStatus.getTaskId().getValue() + " state is " + taskStatus.getState());
+		if(taskStatus.getState().equals(Protos.TaskState.TASK_FINISHED)){
+			tasksCompleted++;
+			int taskResult = Integer.parseInt(taskStatus.getData().toStringUtf8());
+			this.totalResult += taskResult;
+			System.out.println("Task " + taskStatus.getTaskId().getValue() + " finished with area " + taskStatus.getMessage());
+		}else{
+			System.out.println("Task " + taskStatus.getTaskId().getValue() + " has message " + taskStatus.getMessage());
+		}
 		
+		
+		if(this.tasksCompleted == this.numTasks){
+			System.out.println("Result is " + this.totalResult);
+			driver.stop();
+		}
 	}
 
 	@Override
@@ -98,6 +133,10 @@ public class PrinterSchedulerState implements Scheduler{
 	public void error(SchedulerDriver driver, String message) {
 		
 		
+	}
+	
+	private Protos.CommandInfo.Builder createCommand(String args) {
+		return Protos.CommandInfo.newBuilder().setValue("java -cp /xxx.jar" + args);//TODO
 	}
 
 }
